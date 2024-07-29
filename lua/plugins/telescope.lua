b59177c4d7641275bd28b78715f61c88f0b1c9bb
Juan Zuñiga <rusted.cor79@gmail.com>
2024-07-29 13:53:21 -0500

    telescope and file browser, active buffers, etc etc added

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,91 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = 'make'
+    }
+  },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_sile_sorter = true,
+        case_mode = "smart_case",
+      },
+    }
+  },
+  config = function(opts)
+    require('telescope').setup(opts)
+    require('telescope').load_extension('fzf')
+  end,
+  keys = {
+    {
+      "<leader>pp",
+      function()
+        require('telescope.builtin').git_files({ show_untracked = true })
+      end,
+      desc = "Telescope Git Files",
+    },
+    {
+      "<leader>pe",
+      function()
+        require("telescope.builtin").buffers()
+      end,
+      desc = "Telescope Buffers",
+    },
+    {
+      "<leader>gs",
+      function ()
+        require("telescope.builtin").git_status()
+      end,
+      desc = "Telescope Git Status",
+    },
+    {
+      "<leader>gb",
+      function ()
+        require("telescope.builtin").git_bcommits()
+      end,
+      desc = "Telescope Git Buffer Commit",
+    },
+    {
+      "<leader>gc",
+      function ()
+        require("telescope.builtin").git_commits()
+      end,
+      desc = "Telescope Git Commits"
+    },
+    {
+      "<leader>gb",
+      function ()
+        require("telescope.builtin").git_branches()
+      end,
+      desc = "Telescope Git Branches",
+    },
+    {
+      "<leader>pf",
+      function ()
+        require("telescope.builtin").find_files()
+      end,
+      desc = "Telescope Find Files",
+    },
+    {
+      "<leader>ph",
+      function ()
+        require("telescope.builtin").help_tags()
+      end,
+      desc = "Telescope Help"
+    },
+    {
+      "<leader>bb",
+      function ()
+        require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true })
+      end,
+      desc = "Telescope file browser"
+    }
+  }
+}
